11dc945032402ea5934fa6bed13c6ba4ae86d9ec
MSAkel <akel.ms93@gmail.com>
2020-05-04 01:43:52 -0400

    Responsiveness and projects section update

--- main.js
+++ main.js
@@ -2,21 +2,28 @@ document.addEventListener("DOMContentLoaded", init)
 
 let currentProj = 0
 let minProject = 0
-let maxProject = 1
+let maxProject = 2
 
 let projects = [
   {
     "number": "01",
     "title": "Electromanager",
-    "details": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati minus voluptate tempora magniaperiam veniam sunt, aliquid totam laborum excepturi animi neque doloremque maiores nostrum facereeveniet nam provident voluptas.",
-    "image": "imgs/phone-electro.png",
+    "details": "Mobile",
+    "image": "imgs/perspective-electro.png",
     "url": "https://play.google.com/store/apps/details?id=com.leadglancestudios.electromanager"
   },
   {
     "number": "02",
     "title": "Melp",
-    "details": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati minus voluptate tempora magniaperiam veniam sunt, aliquid totam laborum excepturi animi neque doloremque maiores nostrum facereeveniet nam provident voluptas.",
-    "image": "imgs/phone-melp.png",
+    "details": "Mobile",
+    "image": "imgs/perspective-electro.png",
+    "url": "https://github.com/stac0092/react-native-final"
+  },
+  {
+    "number": "03",
+    "title": "Pizza Shop",
+    "details": "Web",
+    "image": "imgs/perspective-electro.png",
     "url": "https://github.com/stac0092/react-native-final"
   }
 ]
@@ -46,6 +53,8 @@ function eventListeners() {
   menuItems.forEach(item => {
     item.addEventListener('click', onClickClose)
   })
+
+  displayProjectsMobile()
 }
 
 function onHoverMenu(){
@@ -150,6 +159,52 @@ function prevProj(){
   }, 400)
 }
 
+function displayProjectsMobile(){
+  let projectsSection = document.querySelector("#project-section .mobile")
+  //let projectContent = document.querySelector(".project-content.mobile")
+
+  projects.forEach(project => {
+    //let projectContainer = document.createElement("div")
+    //let projectContent = document.createElement("div")
+    let imageContainer = document.createElement("div")
+
+    let projNumber = document.createElement("h1")
+    // let projTitle = document.createElement("h1")
+    let projTitle1 = document.createElement("a")
+    // let projType = document.createElement("p")
+    let projImage = document.createElement("img")
+    let projURL = document.createElement("a")
+
+    // projNumber.textContent = project.number
+    // projTitle.textContent = project.title
+    // projTitle.textContent = `${project.number} ${project.title}`
+    projTitle1.textContent = `${project.number} ${project.title}`
+    // projType.textContent = project.details
+    projImage.src = project.image
+    projURL.textContent = "View Project"
+    projURL.href = project.url
+
+    projNumber.classList.add("project-number")
+    projURL.classList.add("cta")
+
+    //projectContent.classList.add("project-description")
+    imageContainer.classList.add("project-image-mobile")
+
+    // projectContent.appendChild(projNumber)
+    // projectContent.appendChild(projTitle)
+    // projectContent.appendChild(projType)
+    imageContainer.appendChild(projImage)
+    imageContainer.appendChild(projTitle1)
+
+    //projectContent.appendChild(imageContainer)
+    //projectContainer.appendChild(projectContent)
+    projectsSection.appendChild(imageContainer)
+  })
+  
+}
+
+
+ScrollReveal().reveal('.project-background-text', { origin:'bottom', delay: 300,opacity: 0,distance:'6%',duration: 1000, mobile: false });
 // Home section animations
 ScrollReveal().reveal('.background-text', { origin:'left', delay: 400,distance:'1%',duration: 1400, mobile: false });
 ScrollReveal().reveal('#hero-title', { origin:'bottom', delay: 1000, opacity: 0,distance:'1%',duration: 1400, mobile: false });
@@ -163,3 +218,7 @@ ScrollReveal().reveal('.about', { origin:'bottom', opacity: 0, distance:'2%',dur
 ScrollReveal().reveal('#project-number', { origin:'left', delay: 300, opacity: 0,distance:'2%',duration: 900, mobile: false });
 ScrollReveal().reveal('.project-description', { origin:'bottom', delay: 400, opacity: 0,distance:'3%',duration: 1200, mobile: false });
 ScrollReveal().reveal('#project-image', { origin:'bottom', opacity: 0, delay: 400, distance:'3%',duration: 1400, mobile: false });
+
+// Contact section animations
+ScrollReveal().reveal('.contact-details', { origin:'bottom', delay: 400, opacity: 0,distance:'3%',duration: 1200, mobile: false });
+ScrollReveal().reveal('.social', { origin:'bottom', opacity: 0, delay: 400, distance:'3%',duration: 1400, mobile: false });
